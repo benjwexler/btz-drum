8b2799ba164e5a0bf2395535662632e7f2b2b6ab
Ben Wexler <mixedywex@gmail.com>
2020-04-26 14:06:36 -0400

    add Div100vh

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../styles/transition.css';
 import MyRouter from "../router/MyRouter";
+import Div100vh from 'react-div-100vh'
 
 const App = () => {
   return (
-    <>
+    <Div100vh>
     <MyRouter />
-    </>
+    </Div100vh>
   );
 }
 
